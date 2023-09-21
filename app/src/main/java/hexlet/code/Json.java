d18cebe721e9253b9e575ff6ee667011c0c2c80f
Artur Sadov <Art.S.V.86@yandex.ru>
2023-09-21 11:41:19 +0400

    Fix

--- app/src/main/java/hexlet/code/Json.java
+++ app/src/main/java/hexlet/code/Json.java
@@ -2,16 +2,18 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
-import java.io.IOException;
 import java.util.Map;
 import java.util.List;
 
 public class Json {
     public static String differenceToJson(List<Map<String, Object>> differenceList) {
-        String result = differenceList.toString();
-        return result;
 
+        try {
+            return new ObjectMapper().writeValueAsString(differenceList);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
