34797439e4bc3baae02c173fc111144a0d20afb5
Artur Sadov <Art.S.V.86@yandex.ru>
2023-09-22 17:04:05 +0400

    Fix

--- app/src/main/java/hexlet/code/DifferenceList.java
+++ app/src/main/java/hexlet/code/DifferenceList.java
@@ -8,23 +8,37 @@ public class DifferenceList {
         keys.addAll(data2.keySet());
         List<Map<String, Object>> differenceList = new LinkedList<>();
         for (String key : keys) {
+            Map<String, Object> node = new HashMap<>();
             Object val1 = (data1.get(key) == null ? "null" : data1.get(key));
             Object val2 = (data2.get(key) == null ? "null" : data2.get(key));
             if (!data2.containsKey(key)) {
-                Map<String, Object> node = Map.of("type", "removed", "key", key,
-                        "value1", data1.get(key));
+//                Map<String, Object> node = Map.of("type", "removed", "key", key,
+//                        "value1", data1.get(key));
+                node.put("key", key);
+                node.put("type", "removed");
+                node.put("value1", val1);
                 differenceList.add(node);
             } else if (!data1.containsKey(key)) {
-                Map<String, Object> node = Map.of("type", "added", "key", key, "value1", data2.get(key));
+// Map<String, Object> node = Map.of("type", "added", "key", key, "value1", data2.get(key));
+                node.put("key", key);
+                node.put("type", "added");
+                node.put("value1", val2);
                 differenceList.add(node);
             } else if (data1.containsKey(key) && data2.containsKey(key)
                     && !val1.equals(val2)) {
-                Map<String, Object> node = Map.of("type", "changed", "key", key,
-                        "value1", val1, "value2", val2);
+//                Map<String, Object> node = Map.of("type", "changed", "key", key,
+//                        "value1", val1, "value2", val2);
+                node.put("key", key);
+                node.put("type", "changed");
+                node.put("value1", val1);
+                node.put("value2", val2);
                 differenceList.add(node);
             } else {
-                Map<String, Object> node = Map.of("type", "unchanged", "key", key,
-                        "value1", data1.get(key));
+//                Map<String, Object> node = Map.of("type", "unchanged", "key", key,
+//                        "value1", data1.get(key));
+                node.put("key", key);
+                node.put("type", "unchanged");
+                node.put("value1", val1);
                 differenceList.add(node);
             }
         }
